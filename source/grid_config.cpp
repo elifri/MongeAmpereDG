b683b4d3e178f314892ca6a77f029d4b3d022263
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2014-03-04 11:02:24 +0100

    move get_nodes to grid_config
    Signed-off-by: Elisa Friebel <elisa.friebel@rwth-aachen.de>

--- source/grid_config.cpp
+++ source/grid_config.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "../include/config.hpp"
+#include "../include/grid_config.hpp"
 
 igpm::configfile* singleton_config_file::cfg;
 
@@ -16,3 +17,19 @@ igpm::configfile& singleton_config_file::instance() {
 	return *cfg;
 }
 
+
+
+void get_nodes(const grid_type &grid, const grid_type::id_type& idLC, nvector_type &nvEigen)
+{
+	Nvector_type nv;
+	grid.nodes(idLC,nv);
+
+	nvEigen.resize(nv.size());
+	for (unsigned int i=0; i< nv.size(); ++i)
+	{
+		for (int j = 0; j < spacedim; j++)
+		{
+			nvEigen(i)(j) = nv[i][j];
+		}
+	}
+}
