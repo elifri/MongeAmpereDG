67f9dde3c6e2af0c83bc930e710b1ad35ca57db2
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2014-04-02 10:24:00 +0200

    Added draft for iteration in FEniCS
    Conflicts:
    	FEniCS/MA_iterated.py

--- FEniCS/MA_iterated.py
+++ FEniCS/MA_iterated.py
@@ -1,28 +1,25 @@
 """
-FEniCS tutorial demo program: Poisson equation with Dirichlet conditions.
-Simplest example of computation and visualization with FEniCS.
-
--Laplace(u) = f on the unit square.
-u = u0 on the boundary.
-u0 = u = 1 + x^2 + 2y^2, f = -6.
 """
 
 from dolfin import *
 import scipy.io
 
 # Create mesh and define function space
-mesh = UnitSquareMesh(1, 1)
+mesh = UnitSquareMesh(40, 40)
 mesh = refine(mesh)
 V = FunctionSpace(mesh, 'DG', 2)
 
 #define poblem
 
 # Define boundary conditions
-u0 = Expression('2*x[0]*x[0] + 2*x[1]*x[1] + 3*x[0]*x[1]')
-#u0 = Constant(0.0)
+#u0 = Expression('2*x[0]*x[0] + 2*x[1]*x[1] + 3*x[0]*x[1]') #simpleMongeAmpere
+#u0 = Constant(0.0) #const rhs
+u0 = Expression('x[0]*x[0]/2.0 + x[1]*x[1]/2.0') #simpleMongeAmpere2
 
 #rhs
-f = Constant(8.0) 
+#f = Constant(-8.0) #simpleMongeAmpere
+#f = Constant(-2.0) #const rhs
+f = Constant(-2.0) #simpleMongeAmpere2
 
 #define exact solution
 u_e = interpolate(u0, V)
@@ -31,30 +28,30 @@ u_e = interpolate(u0, V)
 
 #start solution
 w = Function(V)
-w = interpolate(u0, V)
+#w = interpolate(u0, V)
+w = interpolate(Expression('x[0]*x[0]/2.0 + x[1]*x[1]/2.0'),V)
 #and the cofactor matrix of its hessian
-#coeff = as_matrix(grad(grad(w)))
-coeff = cofac (grad(grad(w)))
+coeff = as_matrix([[1,0],[0,1]])
+#coeff = cofac (grad(grad(w)))
 
 #create output in right space
-wxx = project(coeff[0,0], FunctionSpace(mesh, 'DG',0))
-wyx = project(coeff[1,0], FunctionSpace(mesh, 'DG',0))
-wxy = project(coeff[0,1], FunctionSpace(mesh, 'DG',0))
-wyy = project(coeff[1,1], FunctionSpace(mesh, 'DG',0))
+#wxx = project(coeff[0,0], FunctionSpace(mesh, 'DG',0))
+#wyx = project(coeff[1,0], FunctionSpace(mesh, 'DG',0))
+#wxy = project(coeff[0,1], FunctionSpace(mesh, 'DG',0))
+#wyy = project(coeff[1,1], FunctionSpace(mesh, 'DG',0))
 
 #plot(f_x)
 
-print wxx.vector().array()
-print wyx.vector().array()
-print wxy.vector().array()
-print wyy.vector().array()
-
+#print wxx.vector().array()
+#print wyx.vector().array()
+#print wxy.vector().array()
+#print wyy.vector().array()
 
 #coeff = as_matrix([[ 4, -3],[ -3, 4]])
 #coeff = as_matrix([[1,0],[0,1]])
 
 #penalty
-sigma = Constant(7.0*20.0)
+sigma = 7.0*50.0*10
 
 #maximum number of iterations
 max_it = 1
@@ -68,17 +65,18 @@ h = CellSize(mesh)
 n = FacetNormal(mesh)
 
 #define bilinear form
-a = -inner(as_matrix(coeff)*nabla_grad(u), nabla_grad(v))*dx \
-  + inner(jump(v,n),avg(coeff*nabla_grad(u)))*dS\
-  + inner(jump(u,n),avg(coeff*nabla_grad(v)))*dS\
-  + sigma('+')/h('+')* jump(u)*jump(v)*dS \
-  + v*inner(n,as_matrix(grad(grad(w)))*nabla_grad(u))*ds \
-  + u*inner(n,as_matrix(grad(grad(w)))*nabla_grad(v))*ds \
-  + sigma/h*v*u*ds
+a = inner(as_matrix(coeff)*nabla_grad(u), nabla_grad(v))*dx \
+  - inner(jump(v,n),avg(coeff*nabla_grad(u)))*dS\
+  - inner(jump(u,n),avg(coeff*nabla_grad(v)))*dS\
+  + Constant(sigma)('+')/h('+')* jump(u)*jump(v)*dS \
+  + Constant(sigma)('+')/h('+')* jump(grad(u),n)*jump(grad(v),n)*dS \
+  - v*inner(n,as_matrix(grad(grad(w)))*nabla_grad(u))*ds \
+  - u*inner(n,as_matrix(grad(grad(w)))*nabla_grad(v))*ds \
+  + Constant(sigma)/h*v*u*ds
+#  + Constant(sigma)/h*inner(grad(v),n)*inner(grad(u),n)*ds
 
 #define rhs functional
-L = inner(f,v)*dx + u0*dot(n,coeff*nabla_grad(v))*ds +sigma/h *u0*v*ds
-
+L = inner(f,v)*dx - u0*dot(n,coeff*nabla_grad(v))*ds +Constant(sigma)/h *u0*v*ds
 
 #iterate
 u = Function(V)
@@ -88,6 +86,7 @@ for iteration in range(0,max_it):
   #update penalty
   sigma = sigma*(iteration+1)*10;
 
+  print sigma
   #dump matrices
 #  A, b = assemble_system(a,L)
 
@@ -104,7 +103,7 @@ for iteration in range(0,max_it):
 
   # Plot solution and mesh
   plot(u)
-  #plot(u-u_e)
+  plot(u-u_e)
   #plot(mesh)
 
   # Dump solution to file in VTK format
@@ -115,13 +114,26 @@ for iteration in range(0,max_it):
   #Hold plot
   interactive()
 
-  #temp_array = u.vector().array() #copy u's coefficients
-  #w.vector().set_local(temp_array)
-  
-  #w=u.copy(deepcopy = True)
-  
   #update w
   w.assign(u)
   
-  print coeff
+  coeff = cofac(grad(grad(w)))
+  
+  #create output in right space
+  wxx = project(coeff[0,0], FunctionSpace(mesh, 'DG',0))
+  wyx = project(coeff[1,0], FunctionSpace(mesh, 'DG',0))
+  wxy = project(coeff[0,1], FunctionSpace(mesh, 'DG',0))
+  wyy = project(coeff[1,1], FunctionSpace(mesh, 'DG',0))
 
+  #plot(f_x)
+
+  print "iteration"+str(iteration)+ " cofactor entries"
+  print wxx.vector().array()
+  print wyx.vector().array()
+  print wxy.vector().array()
+  print wyy.vector().array()
+
+  scipy.io.savemat('wxx'+str(iteration)+'.mat', {'wxx': wxx.vector().array()})
+  scipy.io.savemat('wxy'+str(iteration)+'.mat', {'wxy': wxy.vector().array()})
+  scipy.io.savemat('wyx'+str(iteration)+'.mat', {'wyx': wyx.vector().array()})
+  scipy.io.savemat('wyy'+str(iteration)+'.mat', {'wyy': wyy.vector().array()})
