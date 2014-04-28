51fb9f92f4c87b9bbf374eccf89a3f46320a9dce
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2014-04-28 10:06:11 +0200

    Try to preserve convexity by adding more edge mid points (seems not to work either)

--- FEniCS/DG Brenner.py
+++ FEniCS/DG Brenner.py
@@ -14,27 +14,35 @@ def mult(trafo,coeffs):
     c.append(y)
   return c
 
-parameters['reorder_dofs_serial'] = False
+#parameters['reorder_dofs_serial'] = False
 
 
 # Create mesh and define function space
-mesh = UnitSquareMesh(20, 20)
-#mesh = refine(mesh)
+n = 5
+
+mesh = UnitSquareMesh(n, n)
+#plot(mesh)
 V = FunctionSpace(mesh, 'DG', 2)
 bigMesh = refine(refine(mesh))
 bigV = FunctionSpace(bigMesh, 'DG', 2)
 
+k = 1
+print float(k)/n
+edgeMesh = RectangleMesh(1-3*float(3*k)/n, 1-float(2*k)/n, 1, 1, 3*k, k)
+edgeV = FunctionSpace(edgeMesh, 'DG', 2)
+edgePlotV = FunctionSpace(refine(refine(edgeMesh)), 'DG', 2)
+
 # Define boundary conditions
-u0 = Constant(0.0) #const rhs
+#u0 = Constant(0.0) #const rhs
 #u0 = Expression('exp( (pow(x[0],2)+pow(x[1],2))/2. )')#MongeAmpere1
-#u0 = Expression('2*x[0]*x[0] + 2*x[1]*x[1] + 3*x[0]*x[1]') #simpleMongeAmpere
+u0 = Expression('2*x[0]*x[0] + 2*x[1]*x[1] + 3*x[0]*x[1]') #simpleMongeAmpere
 #u0 = Expression('x[0]*x[0]/2.0 + x[1]*x[1]/2.0') #simpleMongeAmpere2
 #u0 = Expression('20*exp(pow(x[0],6)/6.0+x[1])')#BrennerEx1
 
 #rhs
 #f = Expression('(1 + x[0]*x[0]+x[1]*x[1]) * exp(x[0]*x[0]+x[1]*x[1])')#MongeAmpere1
-#f = Constant(4.0)#simpleMongeAmpere
-f = Constant(1.0) #simpleMongeAmpere2
+f = Constant(7.0)#simpleMongeAmpere
+#f = Constant(1.0) #simpleMongeAmpere2
 #f = Expression('2000*pow(exp(pow(x[0],6)/6+x[1]),2)*pow(x[0],4)')#BrennerEx1
 
 #define exact solution
@@ -60,22 +68,22 @@ n = FacetNormal(mesh)
 
 #start solution via laplace
 #penalty
-sigma = 20
+sigma = 200*7
 
 w = interpolate(Expression('x[0]*x[0]/2.0 + x[1]*x[1]/2.0'),V)
-coeff = as_matrix([[1,0],[0,1]])
-#coeff = as_matrix([[4,-3],[-3,4]])
+#coeff = as_matrix([[1,0],[0,1]])
+coeff = as_matrix([[4,-3],[-3,4]])
 
 #define bilinear form
-a = inner(as_matrix(coeff)*nabla_grad(u), nabla_grad(v))*dx \
+a = inner(coeff*nabla_grad(v), nabla_grad(u))*dx \
   - inner(jump(v,n),avg(coeff*nabla_grad(u)))*dS\
   - inner(jump(u,n),avg(coeff*nabla_grad(v)))*dS\
   + Constant(sigma)('+')/h('+')* jump(u)*jump(v)*dS \
-  + Constant(sigma)('+')/h('+')* jump(grad(u),n)*jump(grad(v),n)*dS \
-  - v*inner(n,as_matrix(grad(grad(w)))*nabla_grad(u))*ds \
-  - u*inner(n,as_matrix(grad(grad(w)))*nabla_grad(v))*ds \
+  - v*inner(n,coeff*nabla_grad(u))*ds \
+  - u*inner(n,coeff*nabla_grad(v))*ds \
   + Constant(sigma)/h*v*u*ds
 #  + Constant(sigma)/h*inner(grad(v),n)*inner(grad(u),n)*ds
+#  + Constant(sigma)('+')/h('+')* jump(grad(u),n)*jump(grad(v),n)*dS \
 
 #define rhs functional
 L = inner(-Constant(2.0)*f,v)*dx - u0*dot(n,coeff*nabla_grad(v))*ds +Constant(sigma)/h *u0*v*ds
@@ -86,12 +94,6 @@ solve(a == L, u_)
 def h(x,y):
   return max([.5*x, y-0.5, 2*x+y-1, -5*x+y-4, 2-10*x*x+10*y*y])
 
-#u_ = interpolate(Expression('x[0]*x[0]/2.0 + x[1]*x[1]/2.0+0.25*sin(pi*x[0]*x[1]*150)'),V)
-#u_ = interpolate(Expression('sin(pi*(x[0]-x[1]))'),V)#/exp(2*(x[0]*x[0]+x[1]*x[1]))+x[0]*x[0]+x[1]*x[1]'),V)
-
-plot(u_, title = 'solution linear polated')
-#plot(u_e, title= 'exact solution')
-plot(project(abs(u_-u_e),bigV), title = 'error in non convexified solution')
 
 #interactive()
 #exit()
@@ -134,6 +136,9 @@ tmpfile = open('points_to_convexify.dat', "w")
 
 dofmap = V.dofmap()
 
+suspicious_begin = 0
+suspicious_end = 3
+
 # Iterate over cells and collect dofs
 for cell in cells(mesh):
   cell_ind = cell.index()
@@ -152,21 +157,50 @@ for cell in cells(mesh):
  
   #calculate lagrange coefficients
   coeffs = [u_array[i] for i in dofmap.cell_dofs(cell_ind)]
-  coeffsLagrange = map(u_, points)
+  coeffsLagrange = map(u_, pointsLagrange)
   
-#  print "coeffs ", coeffs
-#  print "coeffsLagrange ", coeffsLagrange
   
   #transform into bezier basis  
   coeffBezier = mult(trafo,coeffs)
+  
+  coeffsdoppelt = mult(trafo_inv, coeffBezier)
 
-#  print "coeffsBezier ", coeffBezier
+  edge_ends = [(0,3), (2,3), (2,4), (1,4), (1,5), (0,5), (3,5), (3,4), (4,5)] 
+
+  middle_pts = [middlePoint(points[i],points[j]) for i,j in edge_ends]
+  middle_val_pts = [middlePoint(coeffBezier[i],coeffBezier[j]) for i,j in edge_ends]
   
-  coeffs = mult(trafo_inv, coeffBezier)
+  #points_with_middle_pts[len(points_with_middle_pts):] = points
+
+  for j in dofmap.cell_dofs(cell_ind):
+    if 569 == j:
+      print "entry 569"
+      print "cell index ", cell_ind
+      print "coeffsLagrange ", coeffsLagrange
+      print "coeffsBezier ", coeffBezier
+      print "coeffs ^2 ", coeffsdoppelt
+      print "coeffs ", coeffs
+
   
+  if cell_ind >= suspicious_begin and cell_ind <= suspicious_end:
+    print "cell index ", cell_ind
+    print "coeffsLagrange ", coeffsLagrange
+    print "coeffsBezier ", coeffBezier
+    print "coeffs ^2 ", coeffsdoppelt
+    print "coeffs ", coeffs
+    print "points ", points
+    print "middle_pts ", middle_pts
+    print "middle_val_pts ", middle_val_pts
+    
   #write into file
   for i in range(len(points)):
-    tmpfile.write(str(points[i][0]) + " " + str(points[i][1]) + " " + str(coeffBezier[i]) + '\n')
+      tmpfile.write("%.10e %.10e %.10e \n" % (points[i][0],points[i][1],coeffBezier[i]))
+
+  for i in range(len(middle_pts)):
+    tmpfile.write("%.10e %.10e %.10e \n" % (middle_pts[i][0],middle_pts[i][1],middle_val_pts[i]))
+    
+
+#    tmpfile.write(str(points[i][0]) + " " + str(points[i][1]) + " " + str(coeffBezier[i]) + '\n')
 
 #close file after writing points  
 tmpfile.close()
@@ -174,7 +208,7 @@ tmpfile.close()
 #write xml output
 File('mesh.xml') << mesh
 File('func_to_convexify.xml') << u_
-
+#exit()
 #convexify with c++ code
 os.system("../bin/Convexify points_to_convexify.dat convexified_points.dat")
 
@@ -184,38 +218,75 @@ tmpfile = open('convexified_points.dat', 'r')
 #create new convex function
 u_convex = Function(V)
 
-coeff_cplusplus = np.array(u_array, copy=True)
+coeff_cplusplus = np.empty(len(u_array))
+#coeff_cplusplus[len(coeff_cplusplus):] = np.array(u_array, copy=True)
+#coeff_cplusplus= []
 u_convex_array = u_convex.vector().array()
+
+print "len u", len(u_array)
+print "cplus plus len ", len(coeff_cplusplus)
+
+
 #update coefficients
 i = 0
+new_coord = 0
+ignore = False
+
 for line in tmpfile:
+  print i, " ", new_coord
+  if ignore:
+    i = i+1
+    if i == 9:
+      ignore = False
+      i = 0
+    continue
   coordinates = line.split(" ")
-  coeff_cplusplus[i] = coordinates[2]
+  print "coord ", coordinates
+  coeff_cplusplus[new_coord] = coordinates[2]
+  new_coord = new_coord +1
   i = i+1
+  if i==6:
+    ignore = True
+    i = 0
+  
+print "coeffs c++ ", coeff_cplusplus
 
 i=0
 #transform from bezier to lagrange basis
 for cell in cells(mesh):
   cell_ind = cell.index()
   coeffs = coeff_cplusplus[i*6: i*6+6]
+  print "22coeffs", coeffs
   coeffs = mult(trafo_inv,coeffs)
   for j in range(6):
     u_convex_array[dofmap.cell_dofs(cell_ind)[j]] = coeffs[j]
+  if cell_ind >= suspicious_begin and cell_ind <= suspicious_end:
+    print "cell index ", cell_ind
+    print "coeffsBezier ", coeff_cplusplus[i*6: i*6+6]
+    print "coeffsLagrange ", coeffs
   i = i+1
+
  
 #print "u_convex after ", u_convex_array
 #print "u_array after ", u_array
 
-#print "difference ", u_array-u_convex_array
+print "max difference ", max(u_array-u_convex_array), " at ", (u_array-u_convex_array).argmax()
 
 u_convex.vector()[:] = u_convex_array
 #u_.vector()[:] = u_array
 
 #plot results
-plot(project(u_convex,bigV), title = 'convexified solution')
 plot(project(u_, bigV), title = 'solution')
-plot(project(abs(u_-u_convex), bigV), title = 'difference convex and not convex')
-plot(project(abs(u_convex-u_e), bigV), title = 'error in conv. sol.')
+plot(project(u_convex,bigV), title = 'convexified solution')
+#plot(project(u_-u_convex, bigV), title = 'difference convex and not convex')
+plot(u_-u_convex, title = 'difference convex and not convex')
+
+#plot(project(u_-u_convex, edgePlotV), title = 'difference convex and not convex on the edge')
+#plot(project(u_, edgePlotV), title = 'solution on the edge')
+#plot(project(u_convex, edgePlotV), title = 'convex. sol on the edge')
+
+#plot(project(abs(u_-u_e),bigV), title = 'error in non convexified solution')
+#plot(project(abs(u_convex-u_e), bigV), title = 'error in conv. sol.')
 
 #plot Determinant of Hessian
 test_convex = det(grad(grad(u_)))
@@ -223,8 +294,13 @@ test_convex2 = det(grad(grad(u_convex)))
 
 bigTest_convex = project(test_convex, bigV)
 bigTest_convex2 = project(test_convex2, bigV)
-plot(bigTest_convex, title = 'determinant of hessian')
-plot(bigTest_convex2, title = 'determinant of convexified hessian')
+plot(test_convex, title = 'determinant of hessian')
+plot(test_convex2, title = 'determinant of convexified hessian')
+
+plot(project(f, bigV), title = 'right-hand side')
+plot(project(abs(f-test_convex), bigV), title = 'rhs - determinant of hessian')
+plot(project(abs(f-test_convex2), bigV), title = 'rhs - determinant of convexified hessian')
+
 
 #hold plot
 interactive()
@@ -232,7 +308,7 @@ interactive()
 #update start solution
 u_.assign(u_convex)
 
-#exit()
+exit()
 
 #====================================
 #define brenner's iteration
