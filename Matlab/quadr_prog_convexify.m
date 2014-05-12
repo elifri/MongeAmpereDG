64bbbd6eb0f22b14db9b4a3198b5a97b7d428445
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2014-05-12 18:48:37 +0200

    Add another cost functional

--- Matlab/quadr_prog_convexify.m
+++ Matlab/quadr_prog_convexify.m
@@ -3,13 +3,20 @@
 %init_C;
 
 %cost functional
-J = @(c) (solution-c)'*(solution-c);
+%J = @(c) (solution-c)'*(solution-c);
 
-n = size(solution,1);
+%n = size(solution,1);
+n = size(C_values, 1);
 
-H = eye(n,n);
-f = -solution;
+% to minimise distance in the coefficients
+%H = eye(n,n);
+%f = -solution;
 
-x0 =x;
+% to minimum distance to data points
+H = A'*A;
+f = -A'*C_values;
+
+opts = optimset('Algorithm','interior-point-convex','Display','iter', 'TolX', 1e-10, 'TolCon', 1e-10);
+
 %solve quadratic program
-[x,fval] = quadprog(H,f,full(C),zeros(size(C,1), 1));
+[x,fval] = quadprog(H,f,full(-C),zeros(size(C,1),1), [],[],[],[],[], opts);
