1b77ccf6b29fb52dba2b05392c9645fcc50502ce
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2014-05-12 09:07:13 +0200

    Add double hash mash type

--- include/grid_config.hpp
+++ include/grid_config.hpp
@@ -34,16 +34,6 @@ struct grid_config_type: public igpm::tgridconfig<grid_config_type,
 
 	static const unsigned int INVALID_OFFSET = (unsigned int) -1;
 
-	// stuff for finding neighbors
-	struct leafcellnodeneigbhor_type {
-		leafcell_type* ptr;   //< pointer to the leaf cell
-		unsigned int   node;  //< node number of the leaf cell
-	};
-
-	typedef pair<id_type,leafcellnodeneigbhor_type> cell_stack_entry_type;
-	typedef std::stack<cell_stack_entry_type> cell_stack_type;
-
-
 };
 
 //------------------------------------------------------------------------------
@@ -69,7 +59,43 @@ typedef grid_type::nodehandle_type        Nhandle_type;
 typedef grid_type::nodehandlevector_type  Nhandlevector_type;
 typedef grid_type::faceidvector_type      Fidvector_type;
 
+// a hash map handling floating values keys with a right interpretation of equalness; value type is value_type
 
+struct hash_Double_Compare;
+
+/**
+ *  Trait class providing hash function and correct
+ *  numerical comparison for space_typs
+ */
+struct hash_Double_Compare{
+
+  /**
+   *  Hash function for space_type
+   *
+   *  @param  key Input tvector<VALUE_TYPE,2> to analyze
+   *  @return Hash value
+   */
+  static unsigned int hash(const space_type& key) {
+    int nExp;
+    return ((unsigned int)(std::frexp(key[0],&nExp)*128.0+0.5))*1
+      +((unsigned int)(std::frexp(key[1],&nExp)*128.0+0.5))*100;
+  }
+
+  /**
+   *  Compare two keys of type tvector<VALUE_TYPE,2>
+   *
+   *  @param  key1 tvector<VALUE_TYPE,2> to analyze
+   *  @param  key2 tvector<VALUE_TYPE,2> to analyze
+   *  @return True, if keys are numerically equal (up to an error of 1e-14)
+   */
+  static bool compareKeys(const space_type& key1, const space_type& key2) {
+    return (std::abs(key1[0]-key2[0])<1e-14)
+      && (std::abs(key1[1]-key2[1])<1e-14);
+  }
+};
+
+typedef igpm::thashcontainer<space_type,value_type,false,false,hash_Double_Compare>	node_hash_map_type;
+typedef igpm::thashcontainer<space_type,int,false,false,hash_Double_Compare>	node_hash_map_int_type;
 
 // counts all existing leafs
 template<class IDTYPEINFO_TYPE>
