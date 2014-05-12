dd363f28d92d040f8a79ee525f94beb7e702ccf4
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2014-05-12 18:49:48 +0200

    tidy up c0 converter

--- include/c0_converter.hpp
+++ include/c0_converter.hpp
@@ -16,20 +16,36 @@
 
 class C0_converter{
 private:
-	node_hash_map_type coefficients;
 	Eigen::VectorXd DG_to_C_indices;
-	node_hash_map_int_type node_indeces;
 
-	void add_coefficient(const space_type &node, const int DG_index, const Eigen::VectorXd & solution, node_hash_map_int_type &count_nodes, int &cur_node_index);
+	//count how many DG elements contribute to a node
+	Eigen::VectorXi dofsDG_to_dofsC_ratio;
+
+	//save node numeration
+	node_hash_map_int_type node_indeces;
+	nvector_type nodes;
+
+
+	void add_dof(const space_type &node, const int DG_index, int &cur_node_index);
 
 public:
-	void init_coefficient_map(grid_type &grid, Eigen::VectorXd &solution);
+	/// initiliaze c0 converter with this grid
+	void init(grid_type &grid, const int number_of_dofs_DG);
 
+	/// deprecated
 	void convert_coefficients(Eigen::VectorXd &Csolution);
+
+	/// converts a vector in DG formulation to C formulation
+	void convert_coefficients_toDG(const Eigen::VectorXd &DGsolution, Eigen::VectorXd &Csolution);
+
+	/// converts a vector in C formulation to DG formulation
 	void convert_coefficients_toC(const Eigen::VectorXd &Csolution, Eigen::VectorXd &solution);
+
+	void convert_matrix_toC(Eigen::SparseMatrix<value_type> &A) const;
 
 	int get_number_of_dofs_C() const;
 
+	void get_node(const int i, space_type &node) const;
 	value_type coeff_at_node(const space_type &node) const;
 
 	///returns the number of dof_G in C enumeration
--- source/c0_converter.cpp
+++ source/c0_converter.cpp
@@ -3,62 +3,43 @@
 
 using namespace std;
 
-void C0_converter::add_coefficient(const space_type &node, const int DG_index, const Eigen::VectorXd & solution, node_hash_map_int_type &count_nodes, int &cur_node_index)
+void C0_converter::add_dof(const space_type &node, const int DG_index, int &cur_node_index)
 {
-	value_type value = solution(DG_index);
-
-	value_type* pcoeff;
-
-	if (!coefficients.find(node, pcoeff))
-	{
-		//add to coefficients
-		coefficients.insert(node, value);
-		//update node count
-		count_nodes.insert(node, 1);
-	}
-	else
-	{
-		cout << " updated " << *pcoeff;
-
-		//add to coefficients
-		*pcoeff += value;
-		//update node count
-		int* counter;
-		count_nodes.find(node, counter);
-		(*counter)++;
-	}
-
-	cout << " to " << *pcoeff << endl;
-
 	int* pindex;
 
 	if (!node_indeces.find(node, pindex))
 	{
 		//store DG to C infos
 		node_indeces.insert(node, cur_node_index);
+		nodes(cur_node_index) = node;
+
 		DG_to_C_indices(DG_index) = cur_node_index;
+		dofsDG_to_dofsC_ratio(cur_node_index)++;
 
 		//assign new index
 		cur_node_index++;
+
 	}
 	else
 	{
 		//store DG to C infos
 		DG_to_C_indices(DG_index) = *pindex;
+		dofsDG_to_dofsC_ratio(*pindex)++;
 	}
+
 }
 
-void C0_converter::init_coefficient_map(grid_type &grid, Eigen::VectorXd &solution)
+void C0_converter::init(grid_type &grid, const int number_of_dofs_DG)
 {
 	assert (degreedim == 2 && shapedim == 6 && " this implementaion works only for triangles and ansatz fct of degree 2!");
 
-	DG_to_C_indices.resize(solution.size());
+	nodes.resize(number_of_dofs_DG);
+
+	DG_to_C_indices.resize(number_of_dofs_DG);
+	dofsDG_to_dofsC_ratio.setZero(number_of_dofs_DG); // careful: we overestimate the entries of dofsDG_...
 
 	//int to count nodes
 	int cur_node_index = 0;
-
-	// count how many DG elements contribute to a node
-	node_hash_map_int_type count_nodes;
 
 	nvector_type nv;
 
@@ -69,90 +50,108 @@ void C0_converter::init_coefficient_map(grid_type &grid, Eigen::VectorXd &soluti
 		const leafcell_type *pLC;
 		grid.findLeafCell(idLC, pLC);
 
-		cout << "handle id " << idLC << endl;
+//		cout << "handle id " << idLC << " with offset " <<  pLC->m_offset<<  endl;
 
 		//get cell nodes
 		get_nodes(grid, idLC, nv);
 
-		//add coefficients in map bezier control points
-		add_coefficient(nv(0), pLC->m_offset, solution, count_nodes, cur_node_index);
-		add_coefficient(nv(1), pLC->m_offset+3, solution, count_nodes, cur_node_index);
-		add_coefficient(nv(2), pLC->m_offset+5, solution, count_nodes, cur_node_index);
-		add_coefficient((nv(0)+nv(1))/2., pLC->m_offset+1, solution, count_nodes, cur_node_index);
-		add_coefficient((nv(0)+nv(2))/2., pLC->m_offset+2, solution, count_nodes, cur_node_index);
-		add_coefficient((nv(2)+nv(1))/2., pLC->m_offset+4, solution, count_nodes, cur_node_index);
+		//add coefficients to bezier control points map
+		add_dof(nv(0), pLC->m_offset, cur_node_index);
+		add_dof((nv(0)+nv(1))/2., pLC->m_offset+1, cur_node_index);
+		add_dof((nv(0)+nv(2))/2., pLC->m_offset+2, cur_node_index);
+		add_dof(nv(1), pLC->m_offset+3, cur_node_index);
+		add_dof((nv(2)+nv(1))/2., pLC->m_offset+4, cur_node_index);
+		add_dof(nv(2), pLC->m_offset+5, cur_node_index);
 
 	}
 
-	//calculate arithmetic middle given coefficients
-	for (node_hash_map_type::iterator it = coefficients.begin(); it != coefficients.end(); ++it)
-	{
-		int* pcount;
+	dofsDG_to_dofsC_ratio.conservativeResize(node_indeces.size());
+	nodes.conservativeResize(node_indeces.size());
 
-		cout << "handle node " << (*it).key() << endl;
-
-		count_nodes.find((*it).key(), pcount);
-		(*it).value()/= (value_type) (*pcount);
-
-		cout << "encounter " << *pcount << " nodes -> new ids " << (*it).value() << endl;
-
-
-		int* pindex;
-		bool found = node_indeces.find((*it).key(), pindex);
-		cout << ((*it).key()).transpose() << " has number " <<  *pindex << endl;
-	}
-
-	cout << "DG to C mapping: " << endl;
+/*	cout << "DG to C mapping: " << endl;
 	for (int i=0; i< DG_to_C_indices.size(); i++)
 	{
 		cout << i << " -> " << DG_to_C_indices(i) << endl;
 	}
 
+	cout << "dofs to dofs C ratio: " << endl;
+	for (int i=0; i< dofsDG_to_dofsC_ratio.size(); i++)
+	{
+		cout << i << " -> " << dofsDG_to_dofsC_ratio(i) << endl;
+	}*/
+
+
 }
 
 int C0_converter::get_number_of_dofs_C() const
 {
-	return coefficients.size();
+	return dofsDG_to_dofsC_ratio.size();
 }
 
-value_type C0_converter::coeff_at_node(const space_type &node) const
-{
-	value_type* pval;
-	if (coefficients.find(node, pval))
-		return *pval;
-	else
-		return 0;
-}
 
 int C0_converter::dof_C(const int dof_DG) const
 {
 	return DG_to_C_indices(dof_DG);
 }
 
-//TODO make it work without init hashmap
-void C0_converter::convert_coefficients(Eigen::VectorXd &Csolution)
+void C0_converter::get_node(const int i, space_type &node) const
 {
-	Csolution.resize(get_number_of_dofs_C());
-	for (node_hash_map_type::iterator it = coefficients.begin(); it != coefficients.end(); ++it)
-	{
-		int* pindex;
-		bool found = node_indeces.find((*it).key(), pindex);
-		assert (found && *pindex < Csolution.size());
-		Csolution(*pindex) = (*it).value();
-	}
+	node = nodes(i);
 }
 
-void C0_converter::convert_coefficients_toC(const Eigen::VectorXd &Csolution, Eigen::VectorXd &solution)
+
+void C0_converter::convert_coefficients_toC(const Eigen::VectorXd &DGsolution, Eigen::VectorXd &Csolution)
+{
+	assert (DGsolution.size() == DG_to_C_indices.size() && "The given vector does not contain as much DG dofs as expected!");
+	Csolution.setZero(get_number_of_dofs_C());
+
+	// calc C dofs; first loop add all corespoding DG dofs, then calc arithmetic middle
+	for (int i = 0; i < DGsolution.size(); i++)
+	{
+		Csolution(DG_to_C_indices(i)) += DGsolution(i);
+//		cout << " add to " << DG_to_C_indices(i) << " coefficient " << DGsolution(i) << " (index " << i << ") -> " << Csolution(DG_to_C_indices(i)) << endl;
+	}
+
+	Csolution = Csolution.cwiseQuotient(dofsDG_to_dofsC_ratio.cast<value_type>());
+//	cout << "to C converted solution " << endl;
+//	cout << Csolution.transpose() << endl;
+
+}
+
+void C0_converter::convert_coefficients_toDG(const Eigen::VectorXd &Csolution, Eigen::VectorXd &DGsolution)
 {
 	assert (Csolution.size() == get_number_of_dofs_C() && "The given vector does not contain as much C dofs as expected!");
 
-	solution.resize(DG_to_C_indices.size());
-	for (int i = 0; i < solution.size(); i++)
+	DGsolution.resize(DG_to_C_indices.size());
+	for (int i = 0; i < DGsolution.size(); i++)
 	{
-		solution(i) = Csolution(DG_to_C_indices(i));
+		DGsolution(i) = Csolution(DG_to_C_indices(i));
 	}
-	cout << "converted solution " << endl;
-	cout << solution.transpose() << endl;
+//	cout << "to DG converted solution " << endl;
+//	cout << DGsolution.transpose() << endl;
+
+}
+
+
+void C0_converter::convert_matrix_toC(Eigen::SparseMatrix<value_type> &A) const
+{
+	std::vector< Eigen::Triplet<value_type> > triplets;
+
+	for (int k=0; k<A.outerSize(); ++k)
+	  for (Eigen::SparseMatrix<value_type>::InnerIterator it(A,k); it; ++it)
+	  {
+		  triplets.push_back( T ( DG_to_C_indices(it.row()), DG_to_C_indices(it.col()), it.value()));
+	  }
+
+	A.resize(get_number_of_dofs_C(), get_number_of_dofs_C());
+	A.setFromTriplets(triplets.begin(), triplets.end());
+
+	for (int k=0; k<A.outerSize(); ++k)
+	  for (Eigen::SparseMatrix<value_type>::InnerIterator it(A,k); it; ++it)
+	  {
+		  it.valueRef() /= dofsDG_to_dofsC_ratio(it.row());
+	  }
+
 
 }
 
