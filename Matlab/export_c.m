5509b7ec54fbdf4b02f1df759e4bad94cbadfc8b
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2015-03-06 11:46:33 +0100

    remove old code

--- Matlab/export_c.m
+++
@@ -1,6 +0,0 @@
-function export_c(x)
-    fprintf('Eigen::VectorXd solution(%d); \n', length(x)); 
-    for i = 1:length(x)
-        fprintf('solution(%d) = %.10f; \n', i-1, x(i));
-    end
-end
--- easymesh/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc -o EasyMesh -O3 easymesh_1_4.c -lm
-gcc -o ShowMesh -O3 showmesh_1_0.c -lm -lX11
