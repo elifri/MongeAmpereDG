91b030ff874428844417bb014e620a52ec771525
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2016-06-01 15:17:27 +0200

    fix hdf5 export

--- src/IO/hdf5Export.hpp
+++ src/IO/hdf5Export.hpp
@@ -28,35 +28,53 @@ void savehdf5(const GridView& grid, const Dune::FieldVector<double,2> &lowerLeft
   const double l_y = upperRight[1] - lowerLeft[1];
 
 
-  int n_x = std::sqrt(grid.size(0)*l_x/l_y*((2*refinement)<< 2));
-  int n_y = grid.size(0)*((2*refinement)<< 2)/n_x;
+  int n_x = std::sqrt(grid.size(0)*l_x/l_y*(1 << (2*refinement)));
+  int n_y = grid.size(0)*(1 << (2*refinement))/n_x;
 
-  const double h_x = ((double)l_x)/(n_x-1);
-  const double h_y = ((double)l_y)/(n_y-1);
+  const double h_x = ((double)l_x)/(n_x);
+  const double h_y = ((double)l_y)/(n_y);
 
-  Array2Dd X(n_x,n_y);
-  Array2Dd Y(n_x,n_y);
+  Array2Dd X(n_x+1,n_y+1);
+  Array2Dd Y(n_x+1,n_y+1);
 
   {   // save points in file after refinement
     for (auto&& element: elements(grid))
     {
       fg.bind(element);
       const auto geometry = element.geometry();
-      for (auto it = PlotRefinementType::vBegin(refinement); it != PlotRefinementType::vEnd(refinement); it++){
 
-        const auto& x_local = it.coords();
-        const auto& x_global = element.geometry().global(x_local);
-        auto transportedX = fg(x_local);
+      if(refinement == 0)
+      {
+        for (int i = 0; i < geometry.corners(); i++)
+        {
+          const auto& x_local = geometry.corner(i);
+          const auto& x_global = element.geometry().global(x_local);
+          auto transportedX = fg(x_local);
 
-        int index_x = (x_global[0]-lowerLeft[0])/h_x;
-        int index_y = (x_global[1]-lowerLeft[1])/h_y; //index of the bottom left corner of the rectangle where x lies in
+          int index_x = (x_global[0]-lowerLeft[0])/h_x;
+          int index_y = (x_global[1]-lowerLeft[1])/h_y; //index of the bottom left corner of the rectangle where x lies in
 
-        //special case at right boundary
-        if (index_x == n_x) index_x--;
-        if (index_y == n_y) index_y--;
+          X(index_x,index_y) = transportedX[0];
+          Y(index_x,index_y) = transportedX[1];
+        }
+      }
+      else
+      {
+        for (auto it = PlotRefinementType::vBegin(refinement); it != PlotRefinementType::vEnd(refinement); it++){
 
-        X(index_x,index_y) = transportedX[0];
-        Y(index_x,index_y) = transportedX[1];
+          const auto& x_local = it.coords();
+          const auto& x_global = element.geometry().global(x_local);
+          auto transportedX = fg(x_local);
+
+          int index_x = (x_global[0]-lowerLeft[0])/h_x;
+          int index_y = (x_global[1]-lowerLeft[1])/h_y; //index of the bottom left corner of the rectangle where x lies in
+
+          assert(isfinite(transportedX[0]));
+          assert(isfinite(transportedX[1]));
+
+          X(index_x,index_y) = transportedX[0];
+          Y(index_x,index_y) = transportedX[1];
+        }
       }
     }
   }
