068e8253372bbdc70ec5e95f8e95d3b21469067c
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2016-07-11 10:13:54 +0200

    add detailed output for convection term analysis

--- src/OT/MA_OT_image_solver.cpp
+++ src/OT/MA_OT_image_solver.cpp
@@ -35,6 +35,229 @@ MA_OT_image_solver::MA_OT_image_solver(const shared_ptr<GridType>& grid, GridVie
    epsMollifier_ = pow((double) epsDivide_, (int) SolverConfig::nonlinear_steps) * epsEnd_;
 }
 
+struct ConvectionFunction{
+
+  typedef MA_OT_image_solver::DiscreteLocalGradientGridFunction LocalGradFunction;
+
+  enum outputVariant{
+    BicubicInterpolation,
+    BSplineInterpolation,
+    OnlyGradientNorm
+  };
+
+  template<typename Solver, typename LOP, typename LOPLinear>
+  ConvectionFunction(std::shared_ptr<LocalGradFunction> &u, const MA_OT_image_Operator_with_Linearisation<Solver, LOP, LOPLinear>& op): localgradu_(u), rhoX(op.f_), rhoY(op.g_) {}
+
+  /**
+   * \brief Bind LocalFunction to grid element.
+   *
+   * You must call this method before evaluate()
+   * and after changes to the coefficient vector.
+   */
+  void bind(const LocalGradFunction::Element& element)
+  {
+    localgradu_->bind(element);
+  }
+
+  double operator()(const LocalGradFunction::Domain& x) const
+  {
+    switch(variant_)
+    {
+    case BicubicInterpolation:
+    {
+      double f_value;
+      rhoX.evaluate(x, f_value);
+
+      auto gradu = (*localgradu_)(x);
+
+      double g_value;
+      FieldVector<double, Config::dim> gradg;
+
+      //calculate illumination at target plane
+      rhoY.evaluate(gradu, g_value);
+      rhoY.evaluateDerivative(gradu, gradg);
+
+      //velocity vector for convection
+      FieldVector<double, Config::dim> b = gradg;
+      b *= -f_value/g_value/g_value;
+
+      return b.two_norm();
+    }
+      break;
+    case BSplineInterpolation:
+    {
+      double f_value;
+      rhoX.evaluate(x, f_value);
+
+      auto gradu = (*localgradu_)(x);
+
+      double g_value;
+      FieldVector<double, Config::dim> gradg;
+
+      //calculate illumination at target plane
+      rhoY.evaluate(gradu, g_value);
+      rhoY.evaluateDerivative(gradu, gradg);
+
+      //velocity vector for convection
+      FieldVector<double, Config::dim> b = gradg;
+      b *= -f_value/g_value/g_value;
+
+      return b.two_norm();
+    }
+      break;
+    case OnlyGradientNorm:
+    {
+      auto gradu = (*localgradu_)(x);
+
+      double g_value;
+      FieldVector<double, Config::dim> gradg;
+      rhoY.evaluateDerivative(gradu, gradg);
+
+      return gradg.two_norm();
+    }
+      break;
+    }
+  }
+
+  void unbind()
+  {
+    localgradu_->unbind();
+  }
+
+  const LocalGradFunction::Element& localContext() const
+  {
+    return localgradu_->localContext();
+  }
+
+  static outputVariant variant_;
+
+  std::shared_ptr<LocalGradFunction> localgradu_;
+  const SmoothImageFunction& rhoX;
+  const SmoothImageFunction& rhoY;
+};
+
+ConvectionFunction::outputVariant ConvectionFunction::variant_ = BicubicInterpolation;
+
+struct TargetFunction{
+
+  typedef MA_OT_image_solver::DiscreteLocalGradientGridFunction LocalGradFunction;
+
+  template<typename Solver, typename LOP, typename LOPLinear>
+  TargetFunction(std::shared_ptr<LocalGradFunction> &u, const MA_OT_image_Operator_with_Linearisation<Solver, LOP, LOPLinear>& op): localgradu_(u), rhoY(op.g_) {}
+
+  /**
+   * \brief Bind LocalFunction to grid element.
+   *
+   * You must call this method before evaluate()
+   * and after changes to the coefficient vector.
+   */
+  void bind(const LocalGradFunction::Element& element)
+  {
+    localgradu_->bind(element);
+  }
+
+  double operator()(const LocalGradFunction::Domain& x) const
+  {
+    if (target_old)
+    {
+      auto gradu = (*localgradu_)(x);
+
+      //calculate illumination at target plane
+      double g_value;
+      rhoY.evaluateNonSmooth(gradu, g_value);
+      return g_value;
+
+    }
+    auto gradu = (*localgradu_)(x);
+
+    //calculate illumination at target plane
+    double g_value;
+    rhoY.evaluate(gradu, g_value);
+    return g_value;
+  }
+
+  void unbind()
+  {
+    localgradu_->unbind();
+  }
+
+  const LocalGradFunction::Element& localContext() const
+  {
+    return localgradu_->localContext();
+  }
+
+  static bool target_old;
+
+  std::shared_ptr<LocalGradFunction> localgradu_;
+  const SmoothImageFunction& rhoY;
+};
+
+bool TargetFunction::target_old = false;
+
+struct EigenValueFunction{
+
+  typedef MA_OT_image_solver::FETraits::DiscreteLocalSecondDerivativeGridFunction LocalHessScalarFunction;
+
+  EigenValueFunction(std::shared_ptr<LocalHessScalarFunction> &u00, std::shared_ptr<LocalHessScalarFunction> &u10,
+      std::shared_ptr<LocalHessScalarFunction> &u01,std::shared_ptr<LocalHessScalarFunction> &u11)
+  {
+    localHessu_[0]= u00;
+    localHessu_[1] =u10;
+    localHessu_[2] =u01;
+    localHessu_[3]=u11;
+  }
+
+  EigenValueFunction(LocalHessScalarFunction &u00, LocalHessScalarFunction &u10,
+      LocalHessScalarFunction &u01, LocalHessScalarFunction &u11)
+  {
+    localHessu_[0]= std::make_shared<LocalHessScalarFunction>(u00);
+    localHessu_[1] = std::make_shared<LocalHessScalarFunction>(u10);
+    localHessu_[2] = std::make_shared<LocalHessScalarFunction>(u01);
+    localHessu_[3]= std::make_shared<LocalHessScalarFunction>(u11);
+  }
+
+
+  /**
+   * \brief Bind LocalFunction to grid element.
+   *
+   * You must call this method before evaluate()
+   * and after changes to the coefficient vector.
+   */
+  void bind(const LocalHessScalarFunction::Element& element)
+  {
+    for (int i= 0; i < localHessu_.size(); i++)
+      localHessu_[i]->bind(element);
+  }
+
+  double operator()(const LocalHessScalarFunction::Domain& x) const
+  {
+    Dune::FieldMatrix<Config::ValueType, Config::dim, Config::dim> Hessu;
+    Hessu[0][0] = (*(localHessu_[0]))(x);
+    Hessu[0][1] = (*(localHessu_[1]))(x);
+    Hessu[0][1] = (*(localHessu_[2]))(x);
+    Hessu[1][1] = (*(localHessu_[3]))(x);
+
+    Config::ValueType ev0, ev1;
+    calculate_eigenvalues(Hessu, ev0, ev1);
+    return std::min(std::abs(ev0), std::abs(ev1));
+  }
+
+  void unbind()
+  {
+    for (int i= 0; i < localHessu_.size(); i++)
+      localHessu_[i]->unbind();
+  }
+
+  const LocalHessScalarFunction::Element& localContext() const
+  {
+    return localHessu_[0]->localContext();
+  }
+
+  std::array<std::shared_ptr<LocalHessScalarFunction>,4> localHessu_;
+};
+
+
+
 void MA_OT_image_solver::plot(const std::string& name) const
 {
   std::cout << "write VTK output? " << writeVTK_ << " ";
@@ -71,6 +294,9 @@ void MA_OT_image_solver::plot(const std::string& name) const
      auto HessianEntry11 = localSecondDerivative(numericalSolution, direction);
      vtkWriter.addVertexData(HessianEntry11 , VTK::FieldInfo("Hessian11", VTK::FieldInfo::Type::scalar, 1));
 
+     EigenValueFunction eigenvalue(HessianEntry00, HessianEntry01, HessianEntry10, HessianEntry11);
+     vtkWriter.addVertexData(eigenvalue, VTK::FieldInfo("SmallestEigenvalue", VTK::FieldInfo::Type::scalar, 1));
+
      //write to file
      std::string fname(plotter.get_output_directory());
      fname += "/"+ plotter.get_output_prefix()+ name + NumberToString(iterations) + ".vtu";
@@ -97,13 +323,58 @@ void MA_OT_image_solver::plot(const std::string& name) const
       fnameOT, op.g_.getOriginalImage().width(), op.g_.getOriginalImage().height());
 */
 
-
   std::string fnamehdf5(plotter.get_output_directory());
   fnamehdf5 += "/"+ plotter.get_output_prefix()+ name + NumberToString(iterations);
 
   savehdf5(*gridView_ptr, setting_.lowerLeft, setting_.upperRight, plotterRefinement_, fnamehdf5, numericalTransportFunction.localFunction());
 
   std::cout << " saved hdf5 file to " << fnamehdf5 << std::endl;
+
+  ConvectionFunction convectionNorm(gradient_u_old, op);
+  convectionNorm.variant_ = ConvectionFunction::BSplineInterpolation;
+
+  std::string fnameConvection(plotter.get_output_directory());
+  fnameConvection += "/"+ plotter.get_output_prefix()+ name + NumberToString(iterations) + "Convection.vtu";
+
+  SubsamplingVTKWriter<GridViewType> vtkWriter(*gridView_ptr,3);
+  vtkWriter.addVertexData(convectionNorm, VTK::FieldInfo("ConvectionNorm", VTK::FieldInfo::Type::scalar, 1));
+
+  TargetFunction interpolatedTargetFunction(gradient_u_old, op);
+  interpolatedTargetFunction.target_old = false;
+
+  vtkWriter.addVertexData(interpolatedTargetFunction, VTK::FieldInfo("interpolTarget", VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter.write(fnameConvection);
+
+  std::cout << " wrote Convection to " << fnameConvection << std::endl;
+
+  std::string fnameGradient(plotter.get_output_directory());
+  fnameGradient += "/"+ plotter.get_output_prefix()+ name + NumberToString(iterations) + "Gradient.vtu";
+  convectionNorm.variant_ = ConvectionFunction::OnlyGradientNorm;
+  SubsamplingVTKWriter<GridViewType> vtkWriter3(*gridView_ptr,3);
+  vtkWriter3.addVertexData(convectionNorm, VTK::FieldInfo("GradientNorm", VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter3.write(fnameGradient);
+  std::cout << " wrote Gradient Norm to " << fnameGradient << std::endl;
+
+  std::string fnameConvectionOld(plotter.get_output_directory());
+  fnameConvectionOld += "/"+ plotter.get_output_prefix()+ name + NumberToString(iterations) + "ConvectionOld.vtu";
+
+  interpolatedTargetFunction.target_old = true;
+  convectionNorm.variant_ = ConvectionFunction::BicubicInterpolation;
+
+  SubsamplingVTKWriter<GridViewType> vtkWriter2(*gridView_ptr,3);
+  vtkWriter2.addVertexData(convectionNorm, VTK::FieldInfo("ConvectionNorm", VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter2.addVertexData(interpolatedTargetFunction, VTK::FieldInfo("interpolTarget", VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter2.write(fnameConvectionOld);
+
+  std::cout << " wrote Convection Old to " << fnameConvectionOld << std::endl;
+
+/*
+  SubsamplingVTKWriter<GridViewType> vtkWriter(*gridView_ptr,2);
+  vtkWriter.addVertexData(interpolatedTargetFunction, VTK::FieldInfo("interpolTarget", VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter.write(fnameTarget);
+*/
+
+
 }
 
 
