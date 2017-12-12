dfcb8d63d508aae3973aca69c93350baa41eb3a7
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2017-12-12 15:16:36 +0100

    do not define manufacture solution

--- include/MAconfig.h
+++ include/MAconfig.h
@@ -23,7 +23,7 @@
 //use analytic derivation (if implemented)
 #define USE_ANALYTIC_JACOBIAN
 
-#define MANUFACTOR_SOLUTION
+//#define MANUFACTOR_SOLUTION
 
 //#define BSPLINES
 //#define USE_C0_PENALTY
