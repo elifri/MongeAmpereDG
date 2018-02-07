71f3a54eee7da0ac2d0d84a12100ed9b01c12afd
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2018-02-07 15:34:25 +0100

    select the correct Operator in case of automatic differentiation

--- include/OT/MA_OT_solver.h
+++ include/OT/MA_OT_solver.h
@@ -52,8 +52,7 @@ public:
 #ifdef USE_ANALYTIC_JACOBIAN
   using Problem_MA_OT_Operator = MA_OT_Operator_with_Linearisation<ProblemTraits<LOP>, Local_Operator_MA_OT_Linearisation>;
 #else
-  //  using Problem_MA_OT_Operator = MA_OT_Operator<ProblemSquareToSquareOperatorTraits<MA_OT_solver,LOP>>;
-//  using Problem_MA_OT_Operator = MA_OT_Operator<ConstOneOperatorTraits<MA_OT_solver,LOP>>;
+  using Problem_MA_OT_Operator = MA_OT_Operator<ProblemTraits<LOP>>;
 #endif
 
 
