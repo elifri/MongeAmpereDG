5e57079a85d9c7fa9b54587df3677ef1765c7978
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2018-04-27 12:01:21 +0200

    make plot of png image according to initial grid

--- include/IO/imageOT.hpp
+++ include/IO/imageOT.hpp
@@ -15,7 +15,7 @@
 #include "ImageFunction.hpp"
 
 template<typename Function, typename DerivativeFunction>
-void print_image_OT(const Function& T, const DerivativeFunction& Jac_T,
+void print_image_OT_backtrace(const Function& T, const DerivativeFunction& Jac_T,
     const ImageFunction& inputImage, const ImageFunction& targetImage,
     const std::string& outputFile, int pixel_width, int pixel_height, int scale = 6)
 {
@@ -40,7 +40,7 @@ void print_image_OT(const Function& T, const DerivativeFunction& Jac_T,
 
       typename DerivativeFunction::Hessian hessU;
       Jac_T.evaluateHess(x, hessU);
-//      transportedPixelvalue/=(hessU[0][0]*hessU[1][1]-hessU[1][0]*hessU[0][1]);
+      transportedPixelvalue/=naive_determinant(hessU);
 
 
       int i_transported, j_transported;
@@ -63,6 +63,48 @@ void print_image_OT(const Function& T, const DerivativeFunction& Jac_T,
 
   transported.save(outputFile.c_str());
 }
+
+template<typename GridView, typename LocalFunction, typename LocalDerivativeFunction>
+void print_image_OT(const GridView& gridView, LocalFunction& T, LocalDerivativeFunction& Jac_T,
+    const ImageFunction& inputImage, const ImageFunction& targetImage,
+    const std::string& outputFile, int pixel_width, int pixel_height, int refinement = 3)
+{
+  const int originalHeight = inputImage.getOriginalImage().height();
+  const int originalWidth = inputImage.getOriginalImage().width();
+  cimg_library::CImg<double> transported(pixel_width,pixel_height);
+
+  transported.fill(0);
+
+  for (auto&& element: elements(gridView))
+  {
+    T.bind(element);
+    Jac_T.bind(element);
+
+    for (auto it = PlotRefinementType::vBegin(refinement); it != PlotRefinementType::vEnd(refinement); it++){
+      const auto x = it.coords();
+      auto transportedX = T(x);
+
+      double transportedPixelvalue;
+      inputImage.evaluate(element.geometry().global(x),transportedPixelvalue); //evaluate f(x)
+
+      typename LocalDerivativeFunction::Hessian hessU;
+      Jac_T.evaluateHess(x, hessU);
+      transportedPixelvalue/=naive_determinant(hessU);
+
+
+      int i_transported, j_transported;
+      targetImage.getPixel(transportedX, i_transported, j_transported);
+      transported(i_transported, j_transported) += transportedPixelvalue;
+    }
+  }
+
+  transported /= targetImage.factor_;
+  transported /= 255.0 / (255.0 + targetImage.minValue());
+  transported -= targetImage.minValue();
+
+  transported.save(outputFile.c_str());
+}
+
 
 
 
