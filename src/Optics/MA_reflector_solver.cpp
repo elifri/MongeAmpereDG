74a5555603c1831a56adb06992a72bce22cb6097
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2019-05-21 10:30:17 +0200

    use less plots and init from cartesian grid file

--- src/Optics/MA_reflector_solver.cpp
+++ src/Optics/MA_reflector_solver.cpp
@@ -5,16 +5,21 @@
  *      Author: friebel
  */
 
-#include <dune/grid/io/file/vtk/vtkwriter.hh>
-#include <dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
-#include <dune/grid/io/file/vtk/common.hh>
-
 
 #include "Optics/MA_reflector_solver.h"
 #include "Optics/init_with_ellipsoid_method.hpp"
 #include "Optics/plot_functions_optics.hpp"
 
 #include "utils.hpp"
+
+
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
+#include <dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
+#include <dune/grid/io/file/vtk/common.hh>
+
+#include <dune/functions/gridfunctions/gridviewfunction.hh>
+
+#include "IO/CartesianOpticExporter.hpp"
 
 
 MA_reflector_solver::MA_reflector_solver(GridHandlerType& gridHandler, const shared_ptr<GridType>& gridTarget,
@@ -26,8 +31,8 @@ MA_reflector_solver::MA_reflector_solver(GridHandlerType& gridHandler, const sha
   this->op = std::make_shared<OperatorType>(*this, setting_);
 
    //adjust light intensity
-   const auto integralLightOut = get_refl_operator().get_actual_g().integrateOriginal();
-   const auto integralLightIn = get_refl_operator().get_actual_f().integrateOriginal();
+   const double integralLightOut = get_refl_operator().get_actual_g().integrateOriginal();
+   const double integralLightIn = get_refl_operator().get_actual_f().integrateOriginal();
    setting_.povRayOpts.lightSourceColor = Eigen::Vector3d::Constant(integralLightOut/integralLightIn*setting_.lightSourceIntensity);
 
    assembler_.set_X0(opticalSetting.lowerLeft);
@@ -62,23 +67,34 @@ void MA_reflector_solver::create_initial_guess()
 //    Rectangular_mesh_interpolator rectangular_interpolator("../inputData/Optic/exactReflectorProjectionSimple.grid");
   //  Rectangular_mesh_interpolator rectangular_interpolator("../inputData/Optic/exactReflectorProjectionSimpleRoentgen.grid");
   //  Rectangular_mesh_interpolator rectangular_interpolator("../inputData/Optic/exactReflectorProjectionSimpleRose.grid");
-  //
-//    assert(is_close(rectangular_interpolator.x_min, setting_.lowerLeft[0], 1e-12));
-//    assert(is_close(rectangular_interpolator.y_min, setting_.lowerLeft[1], 1e-12));
+    Rectangular_mesh_interpolator rectangular_interpolator("/home/disk/friebel/temp/initial_guess/OneParametrised.data");
 
-  //  Rectangular_mesh_interpolator rectangular_interpolatorDerX("../inputData/exactReflectorProjectionSimpleDerX.grid");
-  //  Rectangular_mesh_interpolator rectangular_interpolatorDerY("../inputData/exactReflectorProjectionSimpleDerY.grid");
+    assert(is_close(rectangular_interpolator.x_min, setting_.lowerLeft[0], 1e-12));
+    assert(is_close(rectangular_interpolator.y_min, setting_.lowerLeft[1], 1e-12));
 
-  //  assert(is_close(rectangular_interpolatorDerX.x_min, SolverConfig::lowerLeft[0], 1e-12));
-  //  assert(is_close(rectangular_interpolatorDerX.y_min, SolverConfig::lowerLeft[1], 1e-12));
-  //  assert(is_close(rectangular_interpolatorDerY.x_min, SolverConfig::lowerLeft[0], 1e-12));
-  //  assert(is_close(rectangular_interpolatorDerY.y_min, SolverConfig::lowerLeft[1], 1e-12));
-  //
-//      project([&rectangular_interpolator](Config::SpaceType x){return 1.0/rectangular_interpolator.evaluate(x);},solution);
+/*
+    Rectangular_mesh_interpolator rectangular_interpolatorDerX("../inputData/exactReflectorProjectionSimpleDerX.grid");
+    Rectangular_mesh_interpolator rectangular_interpolatorDerY("../inputData/exactReflectorProjectionSimpleDerY.grid");
+*/
+    Rectangular_mesh_interpolator rectangular_interpolatorDerX("/home/disk/friebel/temp/initial_guess/OneParametrisedDx.data");
+    Rectangular_mesh_interpolator rectangular_interpolatorDerY("/home/disk/friebel/temp/initial_guess/OneParametrisedDy.data");
+
+    assert(is_close(rectangular_interpolatorDerX.x_min, SolverConfig::lowerLeft[0], 1e-12));
+    assert(is_close(rectangular_interpolatorDerX.y_min, SolverConfig::lowerLeft[1], 1e-12));
+    assert(is_close(rectangular_interpolatorDerY.x_min, SolverConfig::lowerLeft[0], 1e-12));
+    assert(is_close(rectangular_interpolatorDerY.y_min, SolverConfig::lowerLeft[1], 1e-12));
+
+
+//    project([&rectangular_interpolator](Config::SpaceType x){return 1./rectangular_interpolator.evaluate(x);},solution);
     const double distance = setting_.initialOpticDistance;
-    const double p = 5;
-//    project([distance](Config::SpaceType x){return distance+x[1];}, solution);
-    project([p, distance](Config::SpaceType x){return distance*std::exp(x.two_norm2()/p);}, solution);
+//    const double p = 5;
+//    project([distance](Config::SpaceType x){return distance+x[0];}, solution);
+//    project([p, distance](Config::SpaceType x){return distance*std::exp(x.two_norm2()/p);}, solution);
+
+    project([&rectangular_interpolator](Config::SpaceType x){return rectangular_interpolator.evaluate(x);},
+	    [&rectangular_interpolatorDerX, &rectangular_interpolatorDerY](Config::SpaceType x){return FieldVector<double, 2>({rectangular_interpolatorDerX.evaluate(x),rectangular_interpolatorDerY.evaluate(x)});},
+	    solution);
+
 
 
 #else
@@ -168,15 +184,27 @@ void MA_reflector_solver::plot(const std::string& name, int no) const
    plotter.writeReflectorPOV(reflPovname, *solution_u_old);
    std::cerr << reflPovname << std::endl;
 
-   //write rhino mesh
-   std::string reflMeshname(plotter.get_output_directory());
-   reflMeshname += "/"+ plotter.get_output_prefix() + name + "reflector" + NumberToString(iterations) + ".3dm";
-   plotter.write_refractor_mesh(reflMeshname, *solution_u_old);
+//   //write rhino mesh
+//   std::string reflMeshname(plotter.get_output_directory());
+//   reflMeshname += "/"+ plotter.get_output_prefix() + name + "reflector" + NumberToString(iterations) + ".3dm";
+//   plotter.write_refractor_mesh(reflMeshname, *solution_u_old);
+//
+//   //write point cloud
+//   std::string reflPointCloudname(plotter.get_output_directory());
+//   reflPointCloudname += "/"+ plotter.get_output_prefix() + name + "reflectorPoints" + NumberToString(iterations) + ".txt";
+//   plotter.save_refractor_points(reflPointCloudname, *solution_u_old);
 
-   //write point cloud
-   std::string reflPointCloudname(plotter.get_output_directory());
-   reflPointCloudname += "/"+ plotter.get_output_prefix() + name + "reflectorPoints" + NumberToString(iterations) + ".txt";
-   plotter.save_refractor_points(reflPointCloudname, *solution_u_old);
+   {
+     VectorType solution_u = solution.segment(0, get_n_dofs_u());
+
+/*      //build gridviewfunction
+     FETraits::DiscreteGridFunction numericalSolution(FEBasisHandler_.uBasis(),solution_u);
+     std::string fnameCartesian(plotter.get_output_directory());
+     fnameCartesian += "/"+ plotter.get_output_prefix()+ name + NumberToString(no) + "outputCartesianGrid.pov";
+
+  //   FETraits::DiscreteGridFunction globalGradU(get_u_old());
+
+   }
 
 }
 
