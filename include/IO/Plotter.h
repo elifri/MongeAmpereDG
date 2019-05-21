94f7496822d7f88295dde7d200330b3153a9923c
Elisa Friebel <elisa.friebel@rwth-aachen.de>
2019-05-21 10:23:32 +0200

    add progressbar while plotting estimated integral

--- include/IO/Plotter.h
+++ include/IO/Plotter.h
@@ -806,9 +806,15 @@ void Plotter::write_points_OT_global(std::ofstream &file, Function &fg, const Gr
 
     int vertex_no = 0;
 
+    int numberOfElement = gridView.size(0);
+    int numberProcessedElements = 0;
+    ProgressBar progressbar;
+    progressbar.start();
+
     {   // save points in file after refinement
       for (auto&& element: elements(gridView))
       {
+	progressbar.status(numberProcessedElements,numberOfElement);
         using ElementType = std::decay_t<decltype(element)>;
         const bool isCube = element.geometry().type().isCube();
 
@@ -821,9 +827,11 @@ void Plotter::write_points_OT_global(std::ofstream &file, Function &fg, const Gr
         {
           write_element_points_OT_global<Function, ElementType, SimplexRefinementType>(element, file, fg, vertex_no, refinement);
         }
+        numberProcessedElements++;
       }
     }
   file << "\t\t\t\t</DataArray>\n" << "\t\t\t</Points>\n";
+  std::cout << " needed " << progressbar.stop()<< " to set up estimate integral data" << std::endl;
 }
 
 
